b77c3a7cb14ab50662424c9e3027e3cb1b7c9bc9
Esteban <estebankaraputny18@gmail.com>
2025-03-27 20:34:33 -0300

    :tada: New: new repository of test

---
+++ index.js
@@ -0,0 +1,12 @@
+function sumar(a, b) {
+    let resultado = a + b;
+}
+
+function mostrarSuma(x, y) {
+    let resultadoSuma = sumar(x);
+
+    console.log("El resultado de la suma es: ", resultadoSuma + total);
+}
+
+mostrarSuma(10, 5);
+// El resultado de la suma es: 15
